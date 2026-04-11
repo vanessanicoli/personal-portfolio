1e321c6b57928c689b3d03c44b4a0606e066519b
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-04-12 01:01:23 +0200

    add footer. project completed. need to update with next projects.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,6 @@
       spaceBetween: 30,
       effect: "fade",
       loop: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
       autoplay: {
         delay: 3000,
         disableOnInteraction: false,
