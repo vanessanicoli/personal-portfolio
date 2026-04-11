c4ca59a960daf512b65bc4b5b75a6772cf7c64e6
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-04-11 23:48:48 +0200

    add section Photography

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Initialize Swiper 
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
